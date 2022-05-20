35012e0a56742c8cc28d0b2b28c1aa35a4315dee
Vaibhav Kamthan <102578216+vk-7397@users.noreply.github.com>
2022-05-20 13:28:39 +0530

    Add files via upload

---
+++ consumeaip-3/src/main/java/com/cg/ApiController.java
@@ -0,0 +1,39 @@
+package com.cg;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ApiController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	private static String url="http://localhost:8080/dispProject";
+	
+	@GetMapping("/getEmployee/{empID}")
+	public List<Object> getEmployeeDataById()
+	
+	{
+		Object[] Employee=restTemplate.getForObject(url,Object[].class);
+		return Arrays.asList(Employee);
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+
+}
