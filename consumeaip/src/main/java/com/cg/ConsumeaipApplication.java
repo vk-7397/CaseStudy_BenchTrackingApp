2773bb15fd9070a8f49d3ef2df364fbc7ebbf2b3
Vaibhav Kamthan <102578216+vk-7397@users.noreply.github.com>
2022-05-20 13:26:39 +0530

    Add files via upload

---
+++ consumeaip/src/main/java/com/cg/ConsumeaipApplication.java
@@ -0,0 +1,33 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ConsumeaipApplication {
+    @Bean
+    public RestTemplate getRestTemplate()
+    
+    {
+		return new RestTemplate();
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    }
+    
+    
+    
+    
+    
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeaipApplication.class, args);
+	}
+
+}
