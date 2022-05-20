57894b7d032d1ef366998b584d744da5637269be
Vaibhav Kamthan <102578216+vk-7397@users.noreply.github.com>
2022-05-20 13:27:31 +0530

    Add files via upload

---
+++ consumeaip-2/src/test/java/com/cg/Consumeaip2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Consumeaip2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
